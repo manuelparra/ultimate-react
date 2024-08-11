b9094f94050e1f794c8588c0005f90afe2f54349
Manuel Parra <manuelparra@live.com.ar>
2024-08-11 15:22:18 +0200

    Efectos / 09 - Reutilizando lógica

--- 07-Efectos/react-app/src/Promesas.tsx
+++ 07-Efectos/react-app/src/Promesas.tsx
@@ -8,19 +8,29 @@ type User = {
 function Promesas() {
   const [users, setUsers] = useState<User[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string>();
+
   useEffect(() => {
     const url = "https://jsonplaceholder.typicode.com/users";
+
     setLoading(true);
     fetch(url)
       .then((response) => {
+        if (!response.ok) throw new Error(`${response.status}`);
+
         return response.json() as Promise<User[]>;
       })
       .then((data) => setUsers(data))
+      .catch((error: Error) => setError(error.message))
       .finally(() => setLoading(false));
   }, []);
 
   if (loading) {
     return <p>Cargando ...</p>;
+  }
+
+  if (error && !loading) {
+    return <p>Ha ocurrido un error {error}</p>;
   }
 
   return (
