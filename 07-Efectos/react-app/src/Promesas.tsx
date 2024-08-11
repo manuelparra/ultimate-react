9eb3204880be622537c5bb1a746959eb05d45ceb
Manuel Parra <manuelparra@live.com.ar>
2024-08-11 11:34:10 +0200

    Efectos / 07 - Estado de carga

--- 07-Efectos/react-app/src/Promesas.tsx
+++ 07-Efectos/react-app/src/Promesas.tsx
@@ -7,15 +7,21 @@ type User = {
 
 function Promesas() {
   const [users, setUsers] = useState<User[]>([]);
-
+  const [loading, setLoading] = useState<boolean>(false);
   useEffect(() => {
     const url = "https://jsonplaceholder.typicode.com/users";
+    setLoading(true);
     fetch(url)
       .then((response) => {
         return response.json() as Promise<User[]>;
       })
-      .then((data) => setUsers(data));
+      .then((data) => setUsers(data))
+      .finally(() => setLoading(false));
   }, []);
+
+  if (loading) {
+    return <p>Cargando ...</p>;
+  }
 
   return (
     <ul>
