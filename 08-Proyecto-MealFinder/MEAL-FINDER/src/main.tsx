6cdb499f3238658278b2b8752ec51d3bcf38d856
Manuel Parra <manuelparra@live.com.ar>
2024-08-15 20:01:28 +0200

    MEAL FINDER - Layout (fixed some issues)

--- 08-Proyecto-MealFinder/MEAL-FINDER/src/main.tsx
+++ 08-Proyecto-MealFinder/MEAL-FINDER/src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
+import { ChakraProvider } from "@chakra-ui/react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { ChakraProvider } from "@chakra-ui/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
