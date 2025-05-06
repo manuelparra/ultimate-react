f9272c24e0cbfb405113c4de0183c7a200f820f0
Manuel Parra <manuelparra@live.com.ar>
2025-05-06 19:05:41 +0200

    Add reducers

--- 09-Gestion-de-estado-global/react-app-reducers/src/App.tsx
+++ 09-Gestion-de-estado-global/react-app-reducers/src/App.tsx
@@ -1,15 +1,33 @@
-import { useState } from "react";
+import { useReducer } from "react";
 
 type Props = {};
 
+type Action = {
+  type: "INCREMENT" | "DECREASE" | "RESET"; // esto es por convención
+};
+
+const reducer = (contador: number, action: Action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return contador + 1;
+    case "DECREASE":
+      return contador - 1;
+    case "RESET":
+      return 0;
+  }
+};
+
 export default function App({}: Props) {
-  const [contador, setContador] = useState(0);
+  const [contador, dispatch] = useReducer(reducer, 0);
+
   return (
     <>
       <h1>Hola Mundo {contador}</h1>
-      <button onClick={() => setContador(contador + 1)}>Incrementar</button>
-      <button onClick={() => setContador(contador - 1)}>Reducir</button>
-      <button onClick={() => setContador(0)}>Reset</button>
+      <button onClick={() => dispatch({ type: "INCREMENT" })}>
+        Incrementar
+      </button>
+      <button onClick={() => dispatch({ type: "DECREASE" })}>Reducir</button>
+      <button onClick={() => dispatch({ type: "RESET" })}>Reset</button>
     </>
   );
 }
