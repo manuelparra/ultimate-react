1d6dee61685f1b9e7858712293509d297b2f4051
Manuel Parra <manuelparra@live.com.ar>
2025-05-14 18:43:47 +0200

    fixed axios folder name

---
+++ 10-Peticiones-react-query/app-react-query-with-axios-mutations/src/App.tsx
@@ -0,0 +1,19 @@
+type Props = {};
+
+function App({}: Props) {
+  return (
+    <>
+      <h2>Post</h2>
+      <form>
+        <div>
+          <input type="text" />
+        </div>
+        <div>
+          <button>Enviar</button>
+        </div>
+      </form>
+    </>
+  );
+}
+
+export default App;
