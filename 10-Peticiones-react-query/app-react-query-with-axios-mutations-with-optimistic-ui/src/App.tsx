4b2ec062a5e8991fbeb6b41172b37ace9682b532
Manuel Parra <manuelparra@live.com.ar>
2025-05-19 22:45:56 +0200

    add optimistic-ui

---
+++ 10-Peticiones-react-query/app-react-query-with-axios-mutations-with-optimistic-ui/src/App.tsx
@@ -0,0 +1,92 @@
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import axios from "axios";
+import { useRef } from "react";
+
+type Props = {};
+
+type Post = {
+  id: number;
+  title: string;
+  body: string;
+  userId: number;
+};
+
+function App({}: Props) {
+  const titleRef = useRef<HTMLInputElement>(null);
+  const bodyRef = useRef<HTMLInputElement>(null);
+
+  const queryClient = useQueryClient();
+
+  const { mutate, isPending, error } = useMutation({
+    mutationFn: (post: Post) =>
+      axios
+        .post<Post>("https://jsonplaceholder.typicode.com/aposts", post)
+        .then((response) => response.data),
+    onMutate: (newPost) => {
+      const oldPosts = queryClient.getQueryData<Post[]>(["posts"]);
+      queryClient.setQueryData<Post[]>(["posts"], (post = []) => [
+        newPost,
+        ...post,
+      ]);
+
+      if (titleRef.current?.value && bodyRef.current?.value) {
+        titleRef.current.value = "";
+        bodyRef.current.value = "";
+      }
+      return oldPosts;
+    },
+    onSuccess: (savedPost, newPost) => {
+      queryClient.setQueryData<Post[]>(["post"], (posts = []) =>
+        posts.map((post) => (post.id === newPost.id ? savedPost : post)),
+      );
+    },
+    onError: (error, newPost, ctx) => {
+      queryClient.setQueryData<Post[]>(["posts"], ctx);
+    },
+  });
+
+  const { data, isLoading } = useQuery({
+    queryKey: ["posts"],
+    queryFn: () =>
+      axios
+        .get<Post[]>("https://jsonplaceholder.typicode.com/posts")
+        .then((response) => response.data),
+  });
+
+  return (
+    <>
+      <h2>Post</h2>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          if (titleRef.current?.value && bodyRef.current?.value) {
+            mutate({
+              id: 0,
+              body: bodyRef.current.value,
+              title: titleRef.current.value,
+              userId: 1,
+            });
+          }
+        }}
+      >
+        <div>
+          <input ref={titleRef} name="title" type="text" placeholder="Título" />
+        </div>
+        <div>
+          <input ref={bodyRef} name="body" type="text" placeholder="Cuerpo" />
+        </div>
+        <div>
+          <button disabled={isPending}>
+            {isPending ? "Creando..." : "Enviar"}
+          </button>
+          <br />
+          {error && <span>{error.message}</span>}
+        </div>
+      </form>
+      {isLoading && <p>Cargando...</p>}
+      <ul>{data?.map((post) => <li key={post.id}>{post.title}</li>)}</ul>
+    </>
+  );
+}
+
+export default App;
