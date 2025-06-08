4b8449abbdd8667938f83214d9aee71ce7673340
Manuel Parra <manuelparra@live.com.ar>
2025-06-08 22:31:40 +0200

    add react router

---
+++ 11-Rutas-React-Router/app-react-router/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div>
+        <h2>Hola mundo! componente App.tsx</h2>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ 11-Rutas-React-Router/app-react-router/src/main.tsx
@@ -0,0 +1,15 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  { path: "/", element: <App /> },
+  { path: "/productos", element: <>mis productos</> },
+]);
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+);
